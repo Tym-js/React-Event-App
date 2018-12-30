eb7da02bdd71113f77043593641450e4937bc137
Tym-js <42276835+Tym-js@users.noreply.github.com>
2018-12-30 10:06:36 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Event-App
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import App from "./app/layout/App.jsx";
+
+const rootEl = document.getElementById("root");
+
+ReactDOM.render(<App />, rootEl);
